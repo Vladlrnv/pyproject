73b0e8344ac42d62dc28501c25ac2ea0fdb0c364
unknown <rodilsyaletom@mail.ru>
2024-08-25 19:37:08 +0800

    add modified

--- src/masks.py
+++ src/masks.py
@@ -1,13 +1,9 @@
 def get_mask_card_number(card_number: str) -> str:
     """Функция маскирует часть номера карты"""
     if len(card_number) == 16 and card_number.isdigit():
-        return (f"{card_number[:4]} {card_number[4:6]}{"**"} {"****"} "
-                f"{card_number[12:]}")
+        return f"{card_number[:4]} {card_number[4:6]}{"**"} {"****"} " f"{card_number[12:]}"
     else:
         return str(None)
-
-
-print(get_mask_card_number("1234567890123456"))
 
 
 def get_mask_account(account_number: str) -> str:
@@ -16,6 +12,3 @@ def get_mask_account(account_number: str) -> str:
         return f"{"**"}{account_number[-4:]}"
     else:
         return str(None)
-
-
-print(get_mask_account("73654108430135874305"))
---
+++ src/widget.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+
+from src.masks import get_mask_account, get_mask_card_number
+
+
+def mask_account_card(user_input: str) -> str:
+    """Функция возвращает строку с замаскированным номером"""
+    if "Счет" in user_input:
+        name = user_input.split()[0]
+        card_number = user_input.split()[1]
+        return f"{name} {get_mask_account(card_number)}"
+
+    elif "Visa" in user_input:
+        name = " ".join(user_input.split()[0:2])
+        card_number = user_input.split()[2]
+        return f"{name} {get_mask_card_number(card_number)}"
+    else:
+        name = user_input.split()[0]
+        card_number = user_input.split()[1]
+        return f"{name} {get_mask_card_number(card_number)}"
+
+
+print(mask_account_card("МИР 7365410843013587"))
+
+
+def get_date(date: str) -> str:
+    """Функция возвращает дату в формате ДД.ММ.ГГГГ"""
+    d = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%f")
+    return d.strftime("%d.%m.%Y")
+    # my_date = date.split("T")
+    # new_date = my_date[0]
+    # is_date = new_date.split("-")
+    # is_date.reverse()
+    # correct_date = ".".join(is_date)
+    # return correct_date
+
+
+print(get_date("2023-12-22T02:26:18.671407"))
